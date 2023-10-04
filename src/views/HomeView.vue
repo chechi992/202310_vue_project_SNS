0902c66710bf95ee8d2c16bc72d368d9d48d84bd
taes60711 <taes60711@gmail.com>
2023-10-04 22:02:23 +0800

    背景色表示される方改善

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,14 @@
 <template>
-  
-  <div class="box-border h-20 w-screen p-4 bg-teal-200">
-    <button
-      class="justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      @click="toLoginView"
-    >
-      toLoginPage
-    </button>
-    <button
-      class="justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      @click="toSettingView"
-    >
-      toSettingPage
-    </button>
+  <div class="">
+      <button class="text-white" @click="toLoginView">toLoginPage</button>
+      <button class="text-white" @click="toSettingView">toSettingPage</button>
+
+      <h3>Count:{{ count }}</h3>
+      <button class="text-white" @click="countPlus">
+        {{ "CountAdd" }}
+      </button>
+      <div class="bg-tahiti py-96 w-1/2">asdsadsa</div>
   </div>
-  <h3>Count:{{ count }}</h3>
-  <button
-    class="justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-    @click="countPlus"
-  >
-    {{"CountAdd"}}
-  </button>
 </template>
 
 <script>
